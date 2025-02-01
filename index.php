9299e93e87ff75695ad3aedad9a7cb4ddd35772f
lurc-zmei <lurc-zmei@inbox.ru>
2025-02-01 14:43:40 +0300

    добавлено условие - если не хватает денег до полного бака

--- index.php
+++ index.php
@@ -14,50 +14,114 @@ function dump($data, $flag = "pr") {
 
 // Резервуары с топливом
 $fuel = [
-	'benzin' => 500,
-	'solyarka' => 400,
-	'diesel' => 450,
+	1 => [
+		'id' => 1,
+		'name' => "benzin",
+		'rest' => 500, // остаток топлива в резервуаре
+		'price' => 7, // цена за единицу топлива
+	],
+	2 => [
+		'id' => 2,
+		'name' => "solyarka",
+		'rest' => 400,
+		'price' => 5.30,
+	],
+	3 => [
+		'id' => 3,
+		'name' => "diesel",
+		'rest' => 450,
+		'price' => 4,
+	],
 ];
 
 // Автомобили, посетившие заправку
 $cars = [
-	[
+	23 => [
+		'id' => 23,
 		'name' => 'nissan',
-		'fuel_type' => 'benzin',
-		'fuel_tank' => 100, // объем бензобака
-		'fuel_rest' => 30, // остаток топлива
+		'wallet' => 527, // количество денег на заправку
+		'fuel' => [
+			'type' => 1, // "внешний ключ" для массива с резервуарами
+			'tank' => 100, // объем бензобака
+			'rest' => 30, // остаток топлива
+		],
 	],
-	[
+	24 => [
+		'id' => 24,
 		'name' => 'renault',
-		'fuel_type' => 'solyarka',
-		'fuel_tank' => 80,
-		'fuel_rest' => 20,
+		'wallet' => 200,
+		'fuel' => [
+			'type' => 2,
+			'tank' => 80,
+			'rest' => 20,
+		],
 	],
-	[
+	25 => [
+		'id' => 25,
 		'name' => 'honda',
-		'fuel_type' => 'diesel',
-		'fuel_tank' => 110,
-		'fuel_rest' => 50,
+		'wallet' => 158,
+		'fuel' => [
+			'type' => 3,
+			'tank' => 110,
+			'rest' => 50,
+		],
 	],
 ];
 
-# Заправка
 
-foreach ($cars as $car) {
-	if ($car['fuel_rest'] <= 40) {
-		
+# Заправка
+foreach ($cars as $id => $car) {
+
+	// если у машины остаток топлива меньше 40, тогда ее заправляем
+	if ($car['fuel']['rest'] <= 40) {
+
 		// объем топлива в резервуаре до заправки
-		$fuel_count_before = $fuel[$car['fuel_type']];
+		$fuel_count_before = $fuel[$car['fuel']['type']]['rest'];
+
 
 		// необходимое количество топлива для заправки полного бака
-		$refuel = $car['fuel_tank'] - $car['fuel_rest']; 
+		$refuel = $car['fuel']['tank'] - $car['fuel']['rest']; 
 
-		// Забираем нужное количество топлива из резервуара для заправки автомобиля
-		$fuel[$car['fuel_type']] = $fuel[$car['fuel_type']] - $refuel;
+
+		// сколько литров можем себе позволить
+		$refuel_possible = floor($car['wallet'] / $fuel[$car['fuel']['type']]['price']);
 		
+
+		/*
+		// стоимость топлива до полного бака
+		//$refuel_cost_tank = $refuel * $fuel[$car['fuel']['type']]['price'];
+
+		// остаток денег после покупки топлива
+		//$count_wallet = fmod($car['wallet'], $fuel[$car['fuel']['type']]['price']); 
+		*/
+
+
+		// если количество топлива мы можем себе позволить больше, чем нужно заправить
+		if($refuel_possible >= $refuel){
+	
+			// покупка топлива
+			$cars[$id]['wallet'] = $car['wallet'] - $refuel * $fuel[$car['fuel']['type']]['price'];
+
+			// Забираем нужное количество топлива из резервуара для заправки автомобиля
+			$fuel[$car['fuel']['type']]['rest'] -= $refuel;
+
+
+		} else {
+
+			// покупка ограниченного количества топлива
+			$cars[$id]['wallet'] = $car['wallet'] - $refuel_possible * $fuel[$car['fuel']['type']]['price'];
+
+			// Забираем нужное количество топлива из резервуара для заправки автомобиля
+			$fuel[$car['fuel']['type']]['rest'] -= $refuel_possible;
+		}
+
 		// Отчет
-		echo "Тип топлива: ".$car['fuel_type'].". Количество изнач: ".$fuel_count_before.". Было дозаправлено: ".$refuel.". Осталось в резервуаре: ".$fuel[$car['fuel_type']]."<br>";
+		echo "
+		Тип топлива: ".$fuel[$car['fuel']['type']]['name']."
+		. Количество изнач: ".$fuel_count_before."
+		. Было дозаправлено: ".$fuel_count_before - $fuel[$car['fuel']['type']]['rest']."
+		. Осталось в резервуаре: ".$fuel[$car['fuel']['type']]['rest']."
+		. Осталось денег:: ". $cars[$id]['wallet'] ."
+		<br>";
 	}
 }
-
-dump($fuel);
